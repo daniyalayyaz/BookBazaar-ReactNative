dfb07666f9a034449d5df52f4156b34d680c9ca8
your_username <your_email_address@example.com>
2022-06-11 19:08:17 +0500

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
+import { IconButton, Colors } from 'react-native-paper';
 
 export default function App() {
   return (
@@ -7,30 +8,52 @@ export default function App() {
         style={styles.stretch}
         source={('https://img-cdn.inc.com/image/upload/w_1920,h_1080,c_fill/images/panoramic/GettyImages-900301626_437925_t2i3bm.jpg')}
       />
+      <View style={styles.title}>
+        <Text style={styles.logintext}>
+          Login
+        </Text>
+      </View>
       <TextInput
         style={styles.input}
+        placeholder="Username or email"
       />
       <TextInput
         style={styles.input}
+        placeholder="Password"
       />
       <View>
-        <Button title='Login' />
+        <Button title='Login' style={styles.btn} color="white" borderColor='#e8b404' />
         <Button title='Forgot Password? Reset here' />
       </View>
       <View
         style={{
           borderBottomColor: 'black',
           borderBottomWidth: 1,
+          borderColor: '#e8b404'
         }}
       />
-      <Text>Or login with</Text>
+      <Text style={styles.loginsubtext}>Or login with</Text>
+      <IconButton
+    icon="camera"
+    color={Colors.red500}
+    size={20}
+    onPress={() => console.log('Pressed')}
+  />
+   <IconButton
+    icon="camera"
+    color={Colors.red500}
+    size={20}
+    onPress={() => console.log('Pressed')}
+  />
       <View
         style={{
           borderBottomColor: 'black',
           borderBottomWidth: 1,
+          borderColor: '#e8b404'
+
         }}
       />
-      <Text>Don't have account</Text>
+      <Text style={styles.loginsubtext}>Don't have account</Text>
       <Button title='Sign up here'/>    </View>
   );
 }
@@ -46,8 +69,37 @@ const styles = StyleSheet.create({
     width: '90%',
     borderRadius: 25,
     margin: 10,
-    border: '#e8b404',
+    borderColor:'#e8b404',
+    borderWidth: 2,
     backgroundColor: '#e8e4e4',
     height: 40,
-  }
+    paddingLeft: 18,
+
+  },
+  title: {
+    width: '50%',
+    textAlign: 'center',
+    borderColor:'#e8b404',
+    borderWidth: 5,
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: '25px',
+    },
+  btn: {
+    borderRadius: '25px',
+    backgroundColor: 'white',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    width: '80%',
+    borderColor:'#e8b404',
+    borderWidth: 5,
+  },
+logintext: {
+  fontSize: 24,
+  fontWeight: 'bold'
+},
+loginsubtext: {
+  fontSize: 20,
+  fontWeight: 'bold'
+}
 });

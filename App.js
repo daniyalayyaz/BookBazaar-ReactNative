459b4397dda9a05c4a579f38ef7e5fe24ca50035
your_username <your_email_address@example.com>
2022-06-11 18:01:30 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+    <Image
+        style={styles.stretch}
+        source={('https://img-cdn.inc.com/image/upload/w_1920,h_1080,c_fill/images/panoramic/GettyImages-900301626_437925_t2i3bm.jpg')}
+      />
+      <TextInput
+        style={styles.input}
+      />
+      <TextInput
+        style={styles.input}
+      />
+      <View>
+        <Button title='Login' />
+        <Button title='Forgot Password? Reset here' />
+      </View>
+      <View
+        style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+        }}
+      />
+      <Text>Or login with</Text>
+      <View
+        style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+        }}
+      />
+      <Text>Don't have account</Text>
+      <Button title='Sign up here'/>    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  stretch: {
+    width: '100%',
+    height: 150,
+    resizeMode: 'stretch',
+  },
+  input: {
+    width: '90%',
+    borderRadius: 25,
+    margin: 10,
+    border: '#e8b404',
+    backgroundColor: '#e8e4e4',
+    height: 40,
+  }
+});
---
+++ Signup.js
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Signup() {
+    return (
+        <View style={styles.container}>
+
+            <TextInput
+                style={styles.input}
+            />
+            <TextInput
+                style={styles.input}
+            />
+            <TextInput
+                style={styles.input}
+            />
+            <TextInput
+                style={styles.input}
+            />
+            <View>
+                <Button title='Sign Up' />
+            </View>
+            <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                }}
+            />
+            <Text>Or login with</Text>
+            <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                }}
+            />
+            <Text>Already have a account?</Text>
+            <Button title='Sign in here' />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    stretch: {
+        width: '100%',
+        height: 150,
+        resizeMode: 'stretch',
+    },
+    input: {
+        width: '90%',
+        borderRadius: 25,
+        margin: 10,
+        border: '#e8b404',
+        backgroundColor: '#e8e4e4',
+        height: 40,
+    }
+});
